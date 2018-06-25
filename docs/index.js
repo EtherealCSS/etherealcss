875fa6719d88c0f7f8699c9d4e3f278e58971d27
Robby <ta7382@gmail.com>
2018-06-25 19:21:32 +0800

    Add highlight code

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,11 @@
 const pres = document.querySelectorAll('pre')
-
 pres.forEach(pre => {
   if (pre.getAttribute('data-type') !== 'html') return
   pre.innerHTML = pre.innerHTML.replace(/</g, `&lt;`).replace(/>/g, `&gt;`)
+})
+
+const codes = document.querySelectorAll('code')
+codes.forEach(code => {
+  if (code.getAttribute('data-type') !== 'html') return
+  code.innerHTML = code.innerHTML.replace(/</g, `&lt;`).replace(/>/g, `&gt;`)
 })
