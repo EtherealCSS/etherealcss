1fcf56ede240ddcca7673e7b7fa8cf90b1529270
Robby <ta7382@gmail.com>
2018-06-25 20:58:44 +0800

    Update highlight

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,2 @@
-const pres = document.querySelectorAll('pre')
-pres.forEach(pre => {
-  if (pre.getAttribute('data-type') !== 'html') return
-  pre.innerHTML = pre.innerHTML.replace(/</g, `&lt;`).replace(/>/g, `&gt;`)
-})
-
-const codes = document.querySelectorAll('code')
-codes.forEach(code => {
-  if (code.getAttribute('data-type') !== 'html') return
-  code.innerHTML = code.innerHTML.replace(/</g, `&lt;`).replace(/>/g, `&gt;`)
-})
+const codes = document.querySelectorAll('pre code.html')
+codes.forEach(code => code.innerHTML = code.innerHTML.replace(/</g, `&lt;`).replace(/>/g, `&gt;`))
